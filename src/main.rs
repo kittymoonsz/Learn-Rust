e69dfd50025eb8588cf957d91217bc721435c384
kitty <iamkittymoon@gmail.com>
2024-11-13 22:37:35 -0300

    erase files and create a simple guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
-use http::Method;
-use http::Request;
-use server::Server;
-
-mod http;
-mod server;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Less => println!("Too small!"),
+        }
+    }
 }
